bc3123df21ff5055dff22a1722e43b34bf1dd647
stanislavpavlovskyi <lichfb@gmail.com>
2023-04-22 21:37:41 -0400

    part-1

--- src/main/java/com/cydeo/TicketingProjectDataApplication.java
+++ src/main/java/com/cydeo/TicketingProjectDataApplication.java
@@ -1,13 +1,20 @@
 package com.cydeo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TicketingProjectDataApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(TicketingProjectDataApplication.class, args);
+    }
+    @Bean
+    public ModelMapper mapper(){
+        return new ModelMapper();
     }
 
 }
